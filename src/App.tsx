6130970d32e29f2aeddf5ac18c2a80d869fcc45a
Renan <renan.fachin@hotmail.com>
2023-05-08 16:32:04 -0300

    Utilizando styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./components/Button"
+
 export function App() {
   return (
-   <>Hello</>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
 
