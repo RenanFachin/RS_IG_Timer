d17d4016b3d4af7d710b91629c8e16eb2186da3e
Renan <renan.fachin@hotmail.com>
2023-05-11 17:01:56 -0300

    Cria um componente de status e finaliza a página de history

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse; //collapse = é para não somar as bordas entre elementos
+
+    min-width: 600px; // Forçando um tamanho para o scroll
+
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
+
+// Fazendo o component de status receber propriedades
+
+// Mapeando as cores
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+// o "as const" serve para definir para o typescript que as variáveis devem ser aquelas e não strings
+
+interface StatusProps {
+  // statusColor: 'yellow' | 'red' | 'green'
+
+  // Não é possível passar o STATUS_COLORS diretamente, é preciso passar keyof typeof
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  /* Adicionando um elemento novo com o css */
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
