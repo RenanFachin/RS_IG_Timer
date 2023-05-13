cb09ea0b847060963ff4f5c2e6fe0ced5db78c4c
Renan <renan.fachin@hotmail.com>
2023-05-13 20:14:12 -0300

    Aplicação finalizada

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useReducer, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 import {
   ActionTypes,
@@ -6,6 +12,7 @@ import {
   MarkCurrentCycleAsFinishedAction,
   addNewCycleAction,
 } from '../reducers/cycles/actions'
+import { differenceInSeconds } from 'date-fns'
 
 // Tipagem da função de criar novo ciclo
 interface CreateCycleData {
@@ -40,14 +47,33 @@ export function CyclesContextProvider({
   const [cyclesState, dispatch] = useReducer(
     cyclesReducer,
     { cycles: [], activeCycleId: null }, // Valores iniciais do reducer
+    (initialState) => {
+      // initialState => é o conteúdo do segundo parâmetro do useReducer
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      )
+
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+
+      // Caso ele não tenha nada no storage
+      return initialState
+    },
   )
 
   const { cycles, activeCycleId } = cyclesState
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
   // Percorrer os ciclos e ver qual item dentro do array tem o mesmo id do ciclo ativo
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
 
   // Função a ser enviada por contexto para atualizar um state
   function markCurrentCycleAsFinished() {
@@ -75,6 +101,13 @@ export function CyclesContextProvider({
   function interruptCurrentCycle() {
     dispatch(InterruptCurrentCycleAction())
   }
+
+  // Salvando os dados no localStorage
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   return (
     <CyclesContext.Provider
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -35,7 +35,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         // Anotando no state o valor de finishedDate para um cycle completado com sucesso
